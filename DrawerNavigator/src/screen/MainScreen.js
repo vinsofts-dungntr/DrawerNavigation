91f5167588823dddf8bef69032b734abf01c8088
vinsofts-dungntr <dungntr@vinsofts.net>
2018-11-30 11:10:48 +0700

    aaa

---
+++ DrawerNavigator/src/screen/MainScreen.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {Image, StyleSheet, Text, View,TouchableOpacity} from 'react-native';
+import {Dimensions} from 'react-native'
+
+
+export default class MenuScreen extends Component {
+  
+  render() {
+    
+    var {height,width}=Dimensions.get('window');
+    return (
+        <View style={{flex:1}}>
+
+          <View style={{height:height/4,justifyContent:'center',alignItems:'center',backgroundColor:'gray'}}>
+            <Image source={require('../../icons/avatar.png')}></Image>
+          </View>
+
+          <View style={{alignItems:'center', backgroundColor:'blue', marginTop:100}}>
+            <Text>Item!</Text>
+          </View>
+
+      </View>
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
